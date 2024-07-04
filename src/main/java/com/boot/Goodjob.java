df5e206d637da45c2a27bad8d2f3dbcfc84ca956
gurwo0535 <gurwo0535@naver.com>
2024-07-04 09:47:11 +0900

    240703

---
+++ src/main/java/com/boot/Goodjob.java
@@ -0,0 +1,9 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+
+public class Goodjob {
+	public static void main(String[] args) {
+		SpringApplication.run(Goodjob.class, args);
+	}
+}
